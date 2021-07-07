2f98a2b4ae48a756550a7ffa448748add077ac61
Thai Thuy <thaithuy24100@gmail.com>
2021-07-07 16:02:00 +0700

    postman

--- BigSchool/BigSchool/Models/AccountViewModels.cs
+++ BigSchool/BigSchool/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace BigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
@@ -109,4 +113,6 @@ namespace BigSchool.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
+
+
 }
